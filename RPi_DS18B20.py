d710a4e7768cd6f857c79b56e915598a9ffed3c2
TimoEwert <ewert.timo@gmail.com>
2021-08-02 20:58:05 +0200

    Nur noch ein Script nötig

--- RPi_DS18B20.py
+++ RPi_DS18B20.py
@@ -5,9 +5,22 @@ import os, sys
 from time import sleep
 import time
 import RPi.GPIO as GPIO
+import board 
+from adafruit_bme280 import basic as adafruit_bme280
 
 deviceID = ['/sys/bus/w1/devices/28-0000073be7d4/w1_slave']
 devicePurpose = ['Temp_ds18b20:']
+
+i2c = board.I2C()  # uses board.SCL and board.SDA
+bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
+bme280.sea_level_pressure = 1013.25
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+red=12
+GPIO.setup(red,GPIO.OUT)
+my_pwm=GPIO.PWM(red,100)
+my_pwm.start(0)
 
 #################################################################################################
 ##### FUNCTIONS #################################################################################
@@ -17,18 +30,45 @@ def setup():
 
 def loop():
   for index in range(len(deviceID)):
-    fan = open("/home/pi/terrapi/fanstatus.txt","r")
-    fanstatus = fan.read()
-    humibme280data = open("/home/pi/terrapi/BME280_humidity_data.txt","r")
-    humidity = humibme280data.read()
-    tempbme280data = open("/home/pi/terrapi/BME280_temperature_data.txt","r")
-    tempbme280 = tempbme280data.read()
-    lcd.printString("W\xE1rme Spot: " + str(getTemp(deviceID[index])+chr(223)+"C"), lcd.LINE_1)
-    lcd.printString("Fanstatus:   " + fanstatus, lcd.LINE_2)
-    lcd.printString("Temperatur:     " + tempbme280+chr(223)+"C", lcd.LINE_3)
-    lcd.printString("Luftfeuchtigkeit:" + humidity + "%", lcd.LINE_4)
-    sleep(2)
-#    lcd.clear()
+  
+    
+    DS18B20 = getTemp(deviceID[index])
+    tempDS18B20=DS18B20
+    HumidityBME280="%0.0f" % bme280.relative_humidity
+    fanstatus=luefter(HumidityBME280)
+    TempBMe280="%0.0f" % bme280.temperature
+    
+    lcd.printString("W\xE1rme Spot: " + tempDS18B20 + chr(223) + "C", lcd.LINE_1)
+    ldc.printString("Fanstatus: " + fanstatus,lcd.LINE_2)
+    lcd.printString("Temperatur: " + TempBMe280 + chr(223) + "C", lcd.LINE_3)
+    lcd.printString("Luftfeuchtigkeit:" + HumidityBME280 + "%", lcd.LINE_4)
+    
+    sleep(1)
+
+def luefter(humidity):
+    humidity=int(humidity)
+    if(humidity >= 90):
+        my_pwm.ChangeDutyCycle(100)
+        fanstatus = ("ON 100%")        
+    elif(humidity >= 80):
+        my_pwm.ChangeDutyCycle(75)
+        fanstatus = ("ON 75%")
+    elif(humidity >= 75):
+        my_pwm.ChangeDutyCycle(50)
+        fanstatus = ("ON 50%")
+    elif(humidity >= 70):
+        my_pwm.ChangeDutyCycle(30)
+        fanstatus = ("ON 30%")
+    elif(humidity < 70):
+        my_pwm.ChangeDutyCycle(0)
+        fanstatus = ("Off 0%")
+    else:
+        fanstatus = ("Off 0%")
+        my_pwm.stop()
+        GPIO.cleanup()
+        sntemp= False
+    
+    return fanstatus
 
 
 def getTemp(ID):
