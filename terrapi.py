e5d7e8826496f04741d4195b478634c10926d5c0
TimoEwert <ewert.timo@gmail.com>
2021-08-06 22:22:35 +0200

    working on code

--- terrapi.py
+++ terrapi.py
@@ -5,23 +5,28 @@ import os, sys
 from time import sleep
 import time
 import RPi.GPIO as GPIO
-import board 
+import board
 from adafruit_bme280 import basic as adafruit_bme280
 import requests
 import configparser
 import datetime
 
+###Edit config file (.env) to change parameters
 config = configparser.ConfigParser()
 config.read(".env")
 
-raintime=config["Configuration"]["raintime"]
-ip_shelly1=config["Configuration"]["ip_shelly1"]
-rainduration=config["Configuration"]["rainduration"]
+raintime=int(config["Configuration"]["raintime"])
+ip_shelly1=config["Configuration"]["ip_shelly1"] ###Shelly for rain
+ip_shelly2=config["Configuration"]["ip_shelly2"] ###Shelly for main light
+ip_shelly3=config["Configuration"]["ip_shelly3"] ###Shelly for heating Spot
+rainduration=int(config["Configuration"]["rainduration"])
 device=config["Configuration"]["device"]
 sea_level_pressure=config["Configuration"]["sea_level_pressure"]
 gpio_for_IRF520=int(config["Configuration"]["gpio_for_IRF520"])
-raintime=int(raintime)
-rainduration=int(rainduration)
+mainlight_on=int(config["Configuration"]["mainlight_on"])
+mainlight_off=int(config["Configuration"]["mainlight_off"])
+heatlight_on=int(config["Configuration"]["heatlight_on"])
+heatlight_off=int(config["Configuration"]["heatlight_off"])
 i2c = board.I2C()  # uses board.SCL and board.SDA
 bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
 bme280.sea_level_pressure = sea_level_pressure
@@ -43,8 +48,9 @@ def setup():
 
 ###Mainloop
 def loop():
-    DS18B20 = getTemp_DS18B20(device)
-    tempDS18B20=DS18B20
+    mainlight_timer()
+    heating_spot_timer()
+    tempDS18B20=getTemp_DS18B20(device)
     rain()
     HumidityBME280="%0.0f" % bme280.relative_humidity
     TempBMe280="%0.0f" % bme280.temperature
@@ -55,17 +61,33 @@ def loop():
     lcd.printString("Luftfeuchtigkeit:" + HumidityBME280 + "%", lcd.LINE_4)
     sleep(1)
 
+###Lightning timer for shelly2 Main Light
+def mainlight_timer():
+    actualtime = datetime.datetime.now()
+    actualtime = int(actualtime.strftime('%H%M')
+    if(actualtime == mainlight_on):
+      requests.get("http://" + ip_shelly2 + "/relay/0?turn=on")
+    elif(actualtime == mainlight_off):
+      requests.get("http://" + ip_shelly2 + "/relay/0?turn=off")
+
+###Lightning timer for shelly3 Heating Spot
+def heating_spot_timer():
+    actualtime = datetime.datetime.now()
+    actualtime = int(actualtime.strftime('&H&M')
+    if(actualtime == heatlight_on):
+      requests.get("http://" + ip_shelly3 + "/relay/0?turn=on")
+    elif(actualtime == heatlight_off):
+      requests.get("http://" + ip_shelly3 + "/relay/0?turn=off")
+
 ###Rain function for Rain at specific times config "raintime" for times to rain
 def rain():
     actualtime = datetime.datetime.now()
-    actualtime = int(actualtime.strftime('%H%M'))
+    actualtime = int(actualtime.strftime('%H%M')
     print(actualtime)
     if(actualtime == raintime):
-      print("hallo")
-#      lcd.printString(actualtime,lcd.LINE_1)
+#      lcd.clear()
 #      lcd.printString(" Bew\xE1sserungsvorgang",lcd.LINE_2)
 #      lcd.printString("L\xF5fter l\xE1uft auf 100%", lcd.LINE_3)
-#      lcd.printString(actualtime, lcd.LINE_4)
       my_pwm.ChangeDutyCycle(0)
       requests.get("http://" + ip_shelly1 + "/relay/0?turn=on")
       sleep(rainduration)
